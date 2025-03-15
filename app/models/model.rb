620e19d5eaa7f21b995d5d64a4de4da3ad28c897
doston9471 <doston.7194@gmail.com>
2025-03-16 00:15:03 +0100

    add Model model

---
+++ app/models/model.rb
@@ -0,0 +1,9 @@
+class Model
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  field :name, type: String
+  field :specifications, type: Hash, default: {}
+  belongs_to :product_line
+
+  validates :name, presence: true
+end
